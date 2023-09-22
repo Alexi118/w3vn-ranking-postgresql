7adae377d839992f7f8951290b005c43b321a369
Alexi118 <cuongnd118@gmail.com>
2023-09-22 17:16:33 +0700

    add rank change to view gplay_ranking

--- 3.createView.sql
+++ 3.createView.sql
@@ -1,14 +1,14 @@
 --CREATE VIEW FOR GPLAY RANKING
 create or replace view gplay_ranking as
 SELECT
-    ROW_NUMBER() OVER (ORDER BY p.elo DESC),p.name, p.race, ROUND(p.elo),
+    ROW_NUMBER() OVER (ORDER BY p.elo DESC) as rank,p.name, p.race, ROUND(p.elo),
     ROUND((CASE WHEN (p.win>0)
           THEN 
                (p.win /(p.win + p.lose)*100)
           ELSE
                0
      END))as winrate,
-     p.win, p.lose, p.social, p.prev_rank, p.player_id
+     p.win, p.lose, p.social, p.prev_rank, p.player_id, ((ROW_NUMBER() OVER (ORDER BY p.elo DESC)) - p.prev_rank) as rankchange
 FROM players p;
 
 --CREATE VIEW FOR RACE STATISTICS
